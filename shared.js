2e105808941b8719fdf579e3c37b908e915eeeae
AnDamazio <damazioandre@hotmail.com>
2022-07-21 11:40:04 -0300

    Add side menu

--- shared.js
+++ shared.js
@@ -1,15 +1,32 @@
 const backdrop = document.querySelector('.backdrop');
 const selectPlanButtons = document.querySelectorAll('.plan button');
 const modal = document.querySelector('.modal');
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
+
 
 modal.addEventListener('click', () => {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
 })
 
 selectPlanButtons.forEach((element) => {
     element.addEventListener('click', function () {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        modal.classList.add('open');
+        backdrop.classList.add('open')
     });
+});
+
+
+toggleButton.addEventListener('click', () => {
+   mobileNav.classList.add('open');
+   backdrop.classList.add('open');
+
+});
+
+backdrop.addEventListener("click", function() {
+        mobileNav.classList.remove('open');
+        backdrop.classList.remove('open');
 });
